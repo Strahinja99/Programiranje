2997b5384fb9a9ed432a27022f46f22571da9768
Strahinja99 <47152662+Strahinja99@users.noreply.github.com>
2019-11-25 22:43:37 +0100

    Add files via upload

---
+++ ispitivanje parnosti i neparnosti/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Strahinja Drinic\n");
+            Console.Write("unesite broj :");
+            int unos = Convert.ToInt32(Console.ReadLine());
+
+            for (; ; )
+            {
+                
+                if (unos % 2 == 0)
+                {
+                    Console.WriteLine("broj je paran\n");
+                }
+                else Console.WriteLine("broj nije paran\n");
+                Console.Write("unesite broj : ");
+                unos = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            
+            
+            
+           
+            
+
+
+        }
+    }
+}
